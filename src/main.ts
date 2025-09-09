36e26886eceb9e46235d7d1f63920bbbb9bb1f30
Tilen Komel <me@komelt.dev>
2025-09-09 23:06:53 +0200

    Update

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import "./assets/style/style.css";
-import "./assets/style/circle.css"
-import "./assets/style/orange.css"
-import App from "./App.vue";
-
-const app = createApp(App);
-
-app.mount("#app");
